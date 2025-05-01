a5162a3ab987066d1c9c7554227202e8378f6cb8
Reon0416 <re16man@icloud.com>
2025-05-02 02:13:41 +0900

    headerにJavaScript書いたよ

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+let lastScrollY=window.scrollY;
+const header=document.getElementById('header');
+
+window.addEventListener('scroll',() => {
+  if(window.scrollY>lastScrollY){
+    header.style.top='-120px';
+  }else{
+    header.style.top=0;
+  }
+  lastScrollY=window.scrollY;
+});
